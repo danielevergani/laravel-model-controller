6672e11430a8d58f3a0fcc10ac693285507b53f6
danielevergani <dvergani94@gmail.com>
2021-05-13 16:53:15 +0200

    creati controller e model e stampato titolo in pagina

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class MovieController extends Controller
 {
+
     public function index(){
-        return view('welcome');
+
+        $movies = Movie::all();
+        
+        return view('welcome', ["movies" => $movies]);
     }
 }
