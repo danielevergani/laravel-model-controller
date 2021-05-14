4d77980c552f59dddf7c978f8acde152d7f7aaf9
danielevergani <dvergani94@gmail.com>
2021-05-14 17:47:47 +0200

    inserito template main e collegato con yield e extend

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,22 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
-
-    <title>Document</title>
-</head>
-<body>
+@extends('movies/template/main')
     <h1>I FILM</h1>
     <ul>
         @foreach ($movies as $movie)
             <li>
                 <h2>{{$movie -> title}}</h2>
+                <button><a href="{{route('movies.show', ['movie' => $movie -> id])}}">DETTAGLI</a></button>
             </li>
         @endforeach
     </ul>
-</body>
-</html>
+
 
