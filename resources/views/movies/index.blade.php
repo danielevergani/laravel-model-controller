6e866f0dfedd2d46b1bd99c7ef58c9cab108c778
danielevergani <dvergani94@gmail.com>
2021-05-18 15:54:52 +0200

    riorganizzata homepage con bootstrap

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,12 +1,36 @@
 @extends('movies/template/main')
+  
+@section('pageTitle')
+    lista film
+@endsection
+
+@section('content')
+<div class="container">
     <h1>I FILM</h1>
-    <ul>
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">titolo</th>
+            <th scope="col">regista</th>
+            <th scope="col">genere</th>
+            <th scope="col">azioni</th>
+          </tr>
+        </thead>
+        <tbody>
+            
+        
         @foreach ($movies as $movie)
-            <li>
-                <h2>{{$movie -> title}}</h2>
-                <button><a href="{{route('movies.show', ['movie' => $movie -> id])}}">DETTAGLI</a></button>
-            </li>
+            <tr>
+                <td>{{$movie -> title}}</td>
+                <td>{{$movie -> author}}</td>
+                <td>{{$movie -> genre}}</td>
+                <td><button class="btn"><a href="{{route('movies.show', ['movie' => $movie -> id])}}">DETTAGLI</a></button></td>
+            </tr>
         @endforeach
-    </ul>
+        </tbody>
+    </table>
+</div>
+@endsection
+    
 
 
--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -1,7 +1,15 @@
 @extends('/movies/template/main')
-    <a href="{{route('movies.index')}}">home page</a>
-    <h1>{{$movie -> title}}</h1>
-    <h3>{{$movie -> author}}</h3>
-    <p>{{$movie -> plot}}</p>
+
+@section('pageTitle')
+    dettagli film {{$movie -> title}}
+@endsection
+
+@section('content')
+<a href="{{route('movies.index')}}">home page</a>
+<h1>{{$movie -> title}}</h1>
+<h3>{{$movie -> author}}</h3>
+<p>{{$movie -> plot}}</p>
+@endsection
+    
 
 
