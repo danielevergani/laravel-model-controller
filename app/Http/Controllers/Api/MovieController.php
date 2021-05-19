50d1eae6da5ef4bf904152169ba8be3e67e34a37
danielevergani <dvergani94@gmail.com>
2021-05-19 16:29:00 +0200

    creati controller e rotte api

---
+++ app/Http/Controllers/Api/MovieController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Movie;
+
+class MovieController extends Controller
+{
+    public function index()
+    {
+        $movies = Movie::all();
+
+        return response()-> json($movies);
+    }
+    public function show(Movie $movie)
+    {   
+        return response()-> json($movie);
+    }
+}
